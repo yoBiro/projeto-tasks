9c08ef4111ca4f76fa8c82ebbf950f62016adb0f
yoBiro <bielzingg92@gmail.com>
2025-07-23 07:04:55 -0300

    Primeiro Commit - Base e Modals

---
+++ app/scripts/index.js
@@ -0,0 +1,13 @@
+const openButton = document.getElementById('#add-task-button');
+const closeButton = document.getElementById('#close-button');
+const modal = document.getElementById('#modal-1');
+
+function openModal() {
+    document.getElementById('modal-1').showModal();
+}
+function closeModal() {
+    document.getElementById('modal-1').close();
+}
+
+function saveModal() {
+}
